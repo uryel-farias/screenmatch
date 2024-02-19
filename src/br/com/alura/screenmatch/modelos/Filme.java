24d4c3c153d3ad28f4003196499fd5748a3c5737
Uryel Farias <uryel.farias@gmail.com>
2024-02-18 22:37:12 -0300

    Começando a trabalhar com herança.

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,45 +1,7 @@
 package br.com.alura.screenmatch.modelos;
 
 public class Filme {
+    private String diretor;
 
-    private String nome;
-    private int anoDeLancamento;
-    private int duracaoEmMinutos;
-    private int totalDeAvaliacoes;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
 
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void exibeFichaTecnica(){
-        System.out.println( "Nome do filme: " + nome);
-        System.out.println( "Ano de Lançamento: " + anoDeLancamento);
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes += 1;
-    }
-
-    public double pegaMedia() {
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-    }
 }
