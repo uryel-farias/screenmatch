b6ae10bb2bbb620acca2636334a5f6970eed2974
Uryel Farias <uryel.farias@gmail.com>
2024-02-21 09:29:03 -0300

    Implementação da classe CalculadoraDeTempo e introdução ao polimorfismo.

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,17 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
+
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("Avatar");
+        meuFilme.setDuracaoEmMinutos(180);
+
+        Filme meuFilme2 = new Filme();
+        meuFilme2.setNome("Matrix");
+        meuFilme2.setDuracaoEmMinutos(120);
 
         Serie lost = new Serie();
         lost.setNome("Lost");
@@ -12,6 +21,12 @@ public class Principal {
         lost.setMinutosPorEpisodio(15);
         lost.exibeFichaTecnica();
         System.out.println("Duração para maratonar essa serie: " + lost.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(meuFilme2);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
 
 
 
